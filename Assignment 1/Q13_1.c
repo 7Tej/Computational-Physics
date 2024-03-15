142f17b95292152f3d426a383004d0ba9c3547e7
7Tej <150845128+7Tej@users.noreply.github.com>
2024-03-15 17:52:30 +0530

    Create Q13_1.c

---
+++ Assignment 1/Q13_1.c
@@ -0,0 +1,56 @@
+//1
+
+#include <stdio.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_linalg.h>
+
+int main() {
+    const int n = 3;
+
+    // 1st matrix
+    double data[3][3] = {
+        {3, -1, 1},
+        {3, 6, 2},
+        {3, 3, 7}
+    };
+
+    // Creating the 1st matrix
+    gsl_matrix_view A = gsl_matrix_view_array(&data[0][0], n, n);
+
+    // Performing LU decomposition
+    gsl_permutation *p = gsl_permutation_alloc(n);
+    int signum;
+    gsl_linalg_LU_decomp(&A.matrix, p, &signum);
+
+    // Output of the decomposed matrix
+    printf("Matrix L:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j > i) {
+                printf("%8.3f ", 0.0);
+            } else if (i == j) {
+                printf("%8.3f ", 1.0);
+            } else {
+                printf("%8.3f ", gsl_matrix_get(&A.matrix, i, j));
+            }
+        }
+        printf("\n");
+    }
+
+    printf("Matrix U:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j >= i) {
+                printf("%8.3f ", gsl_matrix_get(&A.matrix, i, j));
+            } else {
+                printf("%8.3f ", 0.0);
+            }
+        }
+        printf("\n");
+    }
+
+    // Freeing memory
+    gsl_permutation_free(p);
+
+    return 0;
+}
