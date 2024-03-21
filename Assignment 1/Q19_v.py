ee5fa2b8d20584f79b517022d8cf31025f54279e
7Tej <150845128+7Tej@users.noreply.github.com>
2024-03-21 17:31:44 +0530

    Create Q19_v.py

---
+++ Assignment 1/Q19_v.py
@@ -0,0 +1,30 @@
+from timeit import default_timer as timer
+import numpy as np
+
+# Defining matrix
+A=np.array([[0,1,1],[0,1,0],[1,1,0],[0,1,0],[1,0,1]])
+
+
+AAT=np.dot(A,np.transpose(A))
+ATA=np.dot(np.transpose(A),A)
+
+start = timer()
+# To Perform Singular Value Decomposition (SVD)
+# U, S, and VT are the left orhtogonal, Diagonal, and right orthogonal matrices respectively
+U, S, VT = np.linalg.svd(A)
+
+end = timer()
+
+Eig= np.linalg.eigvals(AAT)
+Eig_=np.linalg.eigvals(ATA)
+print('Eigenvalues of AAT=',Eig)
+print('Eigenvalues of ATA=',Eig_)
+
+print("U (left singular vectors):\n", U)
+print("S (singular values):\n", S)
+print("VT (right singular vectors):\n", VT)
+
+E= S**2
+print('Eigvals of either AAT or ATA if SVD is correct=',S**2)
+
+print('Computation time=',end - start,'seconds')
