71367975cbc8af428d4228fb64c9e3bb817b7b71
7Tej <150845128+7Tej@users.noreply.github.com>
2024-04-17 07:27:45 +0530

    Update Q1i.py

--- Assignment 2/Q1i.py
+++ Assignment 2/Q1i.py
@@ -1,43 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Defining the ODE function
-def f(x, y):
-    return -9 * y
-
-# True solution function
+#To Define the true solution function
 def true_solution(x):
-    return np.exp(1 - 9 * x)
+    return np.exp(1 - 9*x)
 
-# Implicit Euler method
-def implicit_euler(f, y0, x0, xn, h):
-    num_steps = int((xn - x0) / h) + 1
-    x_values = np.linspace(x0, xn, num_steps)
-    y_values = np.zeros_like(x_values)
-    y_values[0] = y0
+#To Define the implicit Euler method function
+def implicit_euler(h, x_initial, x_final, y_initial):
+    num_steps = int((x_final - x_initial) / h) + 1
+    x_values = np.linspace(x_initial, x_final, num_steps)
+    y_values = np.zeros(num_steps)
+    y_values[0] = y_initial
 
     for i in range(1, num_steps):
-        y_values[i] = y_values[i - 1] / (1 + 9 * h)
+        y_values[i] = y_values[i-1] / (1 + 9*h)
 
     return x_values, y_values
 
-# Defining initial conditions and step size
-x0, xn = 0, 1
-y0 = np.exp(1)
-h = 0.001  # Step size
+# Given initial conditions
+x_initial = 0
+x_final = 1
+y_initial = np.exp(1)
+h = 0.001
 
-# To Solve using implicit Euler method
-x_values, y_values = implicit_euler(f, y0, x0, xn, h)
+#To Calculate using implicit Euler method
+x_values, y_values_approx = implicit_euler(h, x_initial, x_final, y_initial)
 
 #To Calculate true solution
-true_y_values = true_solution(x_values)
+y_values_true = true_solution(x_values)
 
-#To Plot results
-plt.plot(x_values, y_values, label='Implicit Euler')
-plt.plot(x_values, true_y_values, label='True Solution', linestyle='--')
+#To Print y values in an array
+print("x values:", x_values)
+print("Approximate y values:", y_values_approx)
+
+#To Plot the results
+plt.plot(x_values, y_values_approx, label='Implicit Euler Approximation')
+plt.plot(x_values, y_values_true, label='True Solution')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Implicit Euler Method vs True Solution')
+plt.title('Implicit Euler Method vs. True Solution')
 plt.legend()
 plt.grid(True)
 plt.show()
