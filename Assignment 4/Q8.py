b67fb4ab858d3c928a557bc7a46dc9a273b8d2d6
7Tej <150845128+7Tej@users.noreply.github.com>
2024-05-26 19:04:29 +0530

    Update Q8.py

--- Assignment 4/Q8.py
+++ Assignment 4/Q8.py
@@ -2,6 +2,7 @@
 from numpy import *
 
 n = 1000000
+
 points = random.uniform(-1,1,size = (n,2))
 
 #To determine whether a point (x,y) is within x^2 + y^2 <=1 
@@ -17,3 +18,22 @@ print('Obtained area of the circle =', A)
 
     o fraction of points inside the circle is points_inside_circle/n
     o times the area of the square gives the area of the circle """
+
+    
+""" Volume of a 10-d hyper sphere """
+
+dimensions = 10
+
+# Generates random points in 10 dimensions within the bounds [-1, 1]
+point = random.uniform(-1, 1, size=(n, dimensions))
+
+# Determines if a point lies within the 10-dimensional unit hypersphere
+inside_hypersphere = sum(point**2, axis=1) <= 1
+
+# Counts the number of points inside the hypersphere
+points_inside_hypersphere = sum(inside_hypersphere)
+
+# Calculates the volume of the 10-dimensional unit hypersphere
+volume_hypersphere = (points_inside_hypersphere / n) * (2**dimensions)
+
+print('Obtained volume of the 10-dimensional unit hypersphere =', volume_hypersphere)
